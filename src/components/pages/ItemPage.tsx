0dbcc53ae9b295fad67f15330c14d6be616bb5dd
Tom Burton <Tom.Burton@Outlook.com>
2022-10-26 00:01:43 -0400

    Item Page loads file Data Correctly

--- src/components/pages/ItemPage.tsx
+++ src/components/pages/ItemPage.tsx
@@ -1,7 +1,10 @@
 import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { ReduxState } from '../../app/reducers';
 import ReduxStore from '../../app/store';
 import { documentActions } from '../../documents/documentSlice';
+import { DocumentDetails } from '../../documents/DocumentTypes';
 import DocumentDetailsForm from '../forms/DocumentDetails';
 
 
@@ -12,9 +15,13 @@ export default function ItemPage()
 
    useEffect(() => {
      ReduxStore.dispatch(documentActions.selectDocumentById(itemId));
-   }, [])
-   
-   return <DocumentDetailsForm pageTitle='Item Details' isVersion={true} 
-            { ...ReduxStore.getState().document }
-   />
+   }, [itemId]);
+
+   const docDeets = useSelector<ReduxState, DocumentDetails>
+                               (state => state.document);
+
+   return <DocumentDetailsForm pageTitle='Item Details' 
+                               editable={true} isVersion={true}
+            { ...docDeets }
+          />
 }
