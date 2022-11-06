6c58954d5980a37ca17f690a4837d5e6cce94a9d
Tom Burton <Tom.Burton@Outlook.com>
2022-11-05 22:47:04 -0400

    add admin checkbox to user form.

--- src/components/forms/UserForm.tsx
+++ src/components/forms/UserForm.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, ReactEventHandler } from 'react';
 import { Dispatch } from 'redux';
 import { connect, useSelector } from 'react-redux';
-import { TextField, MenuItem, Button, ClassNameMap, Checkbox, FormControlLabel } from '@mui/material';
+import { TextField, MenuItem, Button, ClassNameMap, Checkbox, FormControlLabel, Tooltip } from '@mui/material';
 import * as yup from 'yup';
 
 import ReduxStore from '../../app/store';
@@ -33,7 +33,7 @@ const UserForm: React.FC<UserFormProps> = (props) =>
   const [name,       setName]       = useState(user.name);
   const [email,      setEmail]      = useState(user.email);
   const [emailError, setEmailError] = useState('');
-  const [isAdmin,    setIsAdmin]    = useState(user.isAdmin);
+  const [isAdmin,    setIsAdmin]    = useState(undefined === user.isAdmin ? false : user.isAdmin);
   const [waa,        setWaa]        = useState(user.waa? user.waa : '' );
   const [userClan,   setClan]       = useState(user.clan? user.clan.name : '');
 
@@ -41,13 +41,13 @@ const UserForm: React.FC<UserFormProps> = (props) =>
     setId(user.id);
     setName(user.name);
     setEmail(user.email);
+    setIsAdmin(undefined === user.isAdmin ? false : user.isAdmin);
     setEmailError(''); //assume valid
     setWaa((user.waa ? user.waa : ''));
     setClan(user.clan? user.clan.name : '');
   }, [user]);
 
   const currentUser = useSelector<ReduxState, Gyet>(state => state.currentUser);
-  
   
   const handleEmailUpdate = (e: string) =>
   {
@@ -106,16 +106,18 @@ const UserForm: React.FC<UserFormProps> = (props) =>
                          value={email} //onChange={(e) => setEmail(e.target.value)} />
                          onChange={e => handleEmailUpdate(e.target.value)}
                          />
-              <FormControlLabel label='Admin'              
-                   control={<Checkbox name='isAdmin' 
-                                      disabled={!currentUser.isAdmin}
-                                      checked={isAdmin}
-                                      //checkedIcon={<AdminPanelSettings />}
-                                      //icon={<AdminPanelSettingsOutlined />}
-                                      onChange={e => setIsAdmin(!!e.target.value)}
-                            />} 
-              />
-                              
+              <Tooltip 
+                 title={`User ${isAdmin? 'has' : 'does not have'} admin access.`} >
+                  <FormControlLabel label='Admin'              
+                      control={<Checkbox name='isAdmin' 
+                      disabled={!currentUser.isAdmin}
+                      checked={!!isAdmin}
+                      checkedIcon={<AdminPanelSettings />}
+                      icon={<AdminPanelSettingsOutlined />}
+                      onChange={e => { setIsAdmin(!isAdmin) } }
+                      />} 
+                  />
+              </Tooltip>                              
            </div>
            <div style={{display: 'inline-grid', maxWidth: '15em'}}>
               <TextField name='waa'   label='Waa' 
