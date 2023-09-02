fcb173f35129e3a9b376e56c67fe1a5f17bc6f2d
Tom Burton <Tom.Burton@Outlook.com>
2023-09-02 15:03:25 -0400

    LoginPage - type fix, and spacing

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,14 +1,15 @@
 import react from "react";
 import { useAppSelector } from "../../app/hooks";
 
-const LoginPage = () => {
+
+const LoginPage = (): JSX.Element => {
 
   const user = useAppSelector(state => state.currentUser);
 
   return <>
-            <h1>Hello {user.name}</h1>
-            <p>If you can read this message you have successfully signed in.</p>
-          </>;
+           <h1>Hello {user.name}</h1>
+           <p>If you can read this message you have successfully signed in.</p>
+         </>;
 }
 
 export default LoginPage;
