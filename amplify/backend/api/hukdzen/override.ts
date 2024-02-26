e618eb4c4c925fe5d090e8f92fff6f112e620914
Tom Burton <Tom.Burton@Outlook.com>
2024-02-25 19:57:50 -0500

    update override to override OpenSearchDefaultPolicy for lambda function
      * parameterize OpenSearchARN

--- amplify/backend/api/hukdzen/override.ts
+++ amplify/backend/api/hukdzen/override.ts
@@ -6,11 +6,34 @@ import {
 export function override(resources: AmplifyApiGraphQlResourceStackTemplate,
                          amplifyProjectInfo: AmplifyProjectInfo)
 {
-   /*
-   // @ t s - i g n o r e
-   resources.opensearch.OpenSearchDomain.index = {
+    /*
+     // @ t s - i g n o r e
+     resources.opensearch.OpenSearchDomain.index = {
      // existing mapping
      doc { keywords: { type: 'keyword' } }
-   };
-   */
+     };
+     */
+    let osPolicy = resources.opensearch.OpenSearchAccessIAMRoleDefaultPolicy;
+
+    //TODO: Properly access the LambdaExecutionRole
+    //osPolicy.roles[ { "Ref": "LambdaExecutionRole" } ];
+
+    const user: string = (`arn:aws:sts::${process.env.ACCOUNT_ID}`
+                          + ':assumed-role/hukdzenLambdaRole20138f42-dev/ingestTrigger-dev');
+    osPolicy.users.push(user);
+
+    const arn = (`arn:aws:es:${process.env.REGION}`
+                     + `:${process.env.ACCOUNT_ID}`
+                     + ':domain/amplify-opense-15yt42dqhtcb4/*');
+
+    const statement = {
+        "Effect":    "Allow",
+        "Action":    ["es:ESHttpGet", "es:ESHttpHead",
+                      "es:ESHttpPut", "es:ESHttpPost"],
+        "Resource":  [arn],
+        "Principal": {"AWS": [user]}
+    };
+
+    //osPolicy.policyDocument.Statement.push(statement);
+    osPolicy.policyDocument.Statement = [statement];
 }
