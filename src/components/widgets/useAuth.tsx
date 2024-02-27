b8fdc791dbef6f2bd4a9e6e14a2becc73e8ae48e
Tom Burton <raystorm_@hotmail.com>
2024-02-26 21:49:49 -0500

    fix build errors

--- src/components/widgets/useAuth.tsx
+++ src/components/widgets/useAuth.tsx
@@ -1,4 +1,4 @@
-import React, {JSX} from 'react'
+import React from 'react'
 import {TextField, View} from '@aws-amplify/ui-react';
 import {Authenticator, useAuthenticator} from '@aws-amplify/ui-react';
 
